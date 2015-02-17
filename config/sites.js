bf57036ddc873076eae7b2f4c7d6c6e61729fe36
Jakob Anderson <janderson@familysearch.org>
2015-02-16 22:48:41 -0700

    added chartjs and got offender items printed on app pages

--- config/sites.js
+++ config/sites.js
@@ -1,15 +1,19 @@
+/**
+ * TODO : turn this into a configurably imported repo
+ */
+
 module.exports = {
   'reference': [
-    'https://familysearch.org/reference',
-    'https://familysearch.org/reference/fsorg',
-    'https://familysearch.org/reference/createApp',
-    'https://familysearch.org/reference/cloneApp',
-    'https://familysearch.org/reference/experiments',
-    'https://familysearch.org/reference/authentication',
-    'https://familysearch.org/reference/debugging',
-    'https://familysearch.org/reference/usingModules',
-    'https://familysearch.org/reference/unitTests',
-    'https://familysearch.org/reference/I18N'
+    'https://familysearch.org/reference/',
+    'https://familysearch.org/reference/fsorg/',
+    'https://familysearch.org/reference/createApp/',
+    'https://familysearch.org/reference/cloneApp/',
+    'https://familysearch.org/reference/experiments/',
+    'https://familysearch.org/reference/authentication/',
+    'https://familysearch.org/reference/debugging/',
+    'https://familysearch.org/reference/usingModules/',
+    'https://familysearch.org/reference/unitTests/',
+    'https://familysearch.org/reference/I18N/'
   ],
   'home': [
     'https://familysearch.org/'
